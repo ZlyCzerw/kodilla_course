df7e03e4ff0e261f8c36b1909e0f7c9fdac78b9d
ZlyCzerw <wiktor.marc@gmail.com>
2019-01-29 09:55:38 +0100

    selenium

--- kodilla-testing2/src/test/java/com/kodill/testing2/crudapp/CrudAppTestSuite.java
+++ kodilla-testing2/src/test/java/com/kodill/testing2/crudapp/CrudAppTestSuite.java
@@ -2,13 +2,18 @@ package com.kodill.testing2.crudapp;
 
 import com.kodilla.testing2.config.WebDriverConfig;
 import org.junit.After;
+import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedCondition;
+import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.Select;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
+import java.util.List;
 import java.util.Random;
 import java.util.stream.Collectors;
 
@@ -50,15 +55,17 @@ public class CrudAppTestSuite {
         return taskName;
     }
 
-    private void sendTestTaskToTrello(String taskname) throws InterruptedException {
+
+    private void sendTestTaskToTrello(String taskName) throws InterruptedException {
         driver.navigate().refresh();
 
         while (!driver.findElement(By.xpath("//select[1]")).isDisplayed());
 
         driver.findElements(By.xpath("//form[@class=\"datatable__row\"]")).stream()
-                .filter(anyForm -> anyForm.findElement(By.xpath(".//p[@class=\"datatable__field-value\"]"))
-                        .getText().equals(taskname))
-                .forEach(theForm -> {WebElement selectElement = theForm.findElement(By.xpath(".//select[1]"));
+                .filter(anyForm -> anyForm.findElement(By.xpath("//p[@class=\"datatable__field-value\"]"))
+                        .getText().equals(taskName))
+                .forEach(theForm -> {
+                    WebElement selectElement = theForm.findElement(By.xpath(".//select[1]"));
                     Select select = new Select(selectElement);
                     select.selectByIndex(1);
 
@@ -67,36 +74,85 @@ public class CrudAppTestSuite {
                 });
         Thread.sleep(5000);
     }
+
+
     @Test
     public void shouldCreateTrelloCard() throws InterruptedException {
-        String taskname = createCrudAppTestTask();
-        sendTestTaskToTrello(taskname);
+        String taskName = createCrudAppTestTask();
+        sendTestTaskToTrello(taskName);
+       // checkTaskExistsInTrello(taskName);
+        shouldDeleteTrelloCard(taskName);
     }
-    private boolean checkTaskExistsInTrello (String taskname) throws InterruptedException {
+
+    private boolean checkTaskExistsInTrello (String taskName) throws InterruptedException {
         final String TRELLO_URL = "https://trello.com/login";
         boolean result = false;
         WebDriver driverTrello = WebDriverConfig.getDriver(WebDriverConfig.FIREFOX);
         driverTrello.get(TRELLO_URL);
 
-        driverTrello.findElement(By.id("user")).sendKeys("");
-        driverTrello.findElement(By.id("password")).sendKeys("");
+        driverTrello.findElement(By.id("user")).sendKeys("wiktorm3");
+        driverTrello.findElement(By.id("password")).sendKeys("Mondeo11");
         driverTrello.findElement(By.id("login")).submit();
 
         Thread.sleep(2000);
 
-        driverTrello.findElements(By.xpath("//a[@class=\"board-title\"]")).stream()
-                .filter(aHref -> aHref.findElements(By.xpath(".//div[@title=\"Kodilla Board\"]")).size()>0)
+        driverTrello.findElements(By.xpath("//a[@class=\"board-tile\"]")).stream()
+                .filter(aHref -> aHref.findElements(By.xpath(".//div[@title=\"Kodilla Application\"]")).size()>0)
                 .forEach(aHref -> aHref.click());
 
         Thread.sleep(2000);
 
         result = driverTrello.findElements(By.xpath("//span")).stream()
-                .filter(theSpan -> theSpan.getText().contains(taskname))
+                .filter(theSpan -> theSpan.getText().contains(taskName))
                 .collect(Collectors.toList())
                 .size()>0;
         driverTrello.close();
-
+        System.out.println(result);
         return result;
+
+    }
+    public void shouldDeleteTrelloCard(String taskName){
+        WebDriverWait wait = new WebDriverWait(driver,2,100);
+        driver.navigate().refresh();
+
+        while (!driver.findElement(By.xpath("//select[1]")).isDisplayed());
+
+//        driver.findElements(By.xpath("//form[@class=\"datatable__row\"]")).stream()
+//                .filter(anyForm -> anyForm.findElement(By.xpath("//p[@class=\"datatable__field-value\"]"))
+//                        .getText().equals(taskName))
+//                .forEach(theForm -> {
+//                    WebElement buttonDeleteCard = theForm.findElement(By.xpath(".//div/fieldset/button[4]"));
+//                    wait.until(ExpectedConditions.elementToBeClickable(buttonDeleteCard)).click();
+//                });
+//
+//
+//        List<WebElement> elementsList = driver.findElements(By.xpath("//form[@class=\"datatable__row\"]")).stream()
+//                .filter(anyForm -> anyForm.findElement(By.xpath("//p[@class=\"datatable__field-value\"]"))
+//                        .getText().equals(taskName)).collect(Collectors.toList());
+//
+//        for(WebElement element: elementsList) {
+//            WebElement buttonDeleteCard = element.findElement(By.xpath(".//div/fieldset/button[4]"));
+//            wait.until(ExpectedConditions.elementToBeClickable(buttonDeleteCard));
+//            buttonDeleteCard.click();
+//        }
+
+        System.out.println("TEN TEKST MA BYC WIDOCZNY");
+        List<WebElement> elementsList = driver.findElements(By.xpath("//div/form[@class=\"datatable__row\"]")).stream()
+                .map(form -> {
+                    System.out.println(form.findElement(By.xpath("//section/div/form/fieldset/p")).getText());
+                    return form;
+                })
+                .filter(anyForm -> anyForm.findElement(By.xpath("//p[@class=\"datatable__field-value\"]"))
+                        .getText().equals(taskName)).collect(Collectors.toList());
+
+        for(WebElement element: elementsList) {
+            WebElement buttonDeleteCard = element.findElement(By.xpath(".//div/fieldset/button[4]"));
+            wait.until(ExpectedConditions.elementToBeClickable(buttonDeleteCard));
+            buttonDeleteCard.click();
+        }
+
+
+        System.out.println("TEN TEKST KONCZY METODE");
 
     }
 }
